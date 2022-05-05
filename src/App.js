caf35858d3ce11c5b6e25be5ca4009cf424bd381
gupta19chirag <gupta1chirag@gmail.com>
2022-05-05 11:09:06 +0530

    local changes version 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React from 'react';
+ //class develope ke.
+class App extends React.Component {
+  constructor(props){
+    super();
+     this.state = {Frequency:100,Name:''};
+//we need to bind every function that is defined.So after creating setVal function I have done this.
+     this.setval = this.setval.bind(this);
+     this.handle1 = this.handle1.bind(this);
+  }
+ 
+  handle1(e){
+   this.setState({Name:e.target.value});
+    
+  }
+  //This function is changing the value when the button defined is pressed.
+  setval(){
+    this.setState({Frequency:500});
+ 
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Welcome to React JS 1   mr {this.props.name}
+          Frequency {this.state.Frequency}
+          Name state{this.state.Name}
         </p>
+      <input value ={this.state.Name} type="text" placeholder ="Enter name" onChange ={this.handle1}></input>
+        <button onClick ={this.setval}>Click me</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,7 +43,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )};
 }
-
+ 
 export default App;

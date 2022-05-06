d403f130de596b44d051fd379320831c931f33ac
gupta19chirag <gupta1chirag@gmail.com>
2022-05-06 10:45:59 +0530

    HELLO

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ root.render(
   </React.StrictMode>
 );
 //Just for jerkins
+//Now from vs-code
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
